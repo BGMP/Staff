8a24bbdd8fe9e256d97dfb5472873eca8d7a201d
José Benavente <jose@bgmp.cl>
2020-08-11 04:53:57 -0400

    Fix typo

--- src/main/java/cl/bgmp/staff/ChatConstant.java
+++ src/main/java/cl/bgmp/staff/ChatConstant.java
@@ -5,7 +5,7 @@ public enum ChatConstant {
   NO_CONSOLE("Debes ser un jugdor para ejecutar este comando."),
   STAFF_MODE_ENABLED("Ahora estás en modo staff."),
   STAFF_MODE_DISABLED("Ya no estás en modo staff."),
-  NOBODY_TO_TELEPORT_TO("No hay nadie a quién teletransportarse."),
+  NOBODY_TO_TELEPORT_TO("No hay nadie a quien teletransportarse."),
   NUMBER_STRING_EXCEPTION("Se esperaba un número. Se ha recibido un string en su lugar."),
   UNKNOWN_ERROR("Ha ocurrido un error desconocido.");
 
