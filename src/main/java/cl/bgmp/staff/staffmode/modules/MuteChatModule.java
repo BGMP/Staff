a31993904b532d1d348f972283dfcbcc16a2e5d6
José Benavente <jose@bgmp.cl>
2020-11-20 05:32:20 -0300

    Fix staffmode chest menu

---
+++ src/main/java/cl/bgmp/staff/staffmode/modules/MuteChatModule.java
@@ -0,0 +1,39 @@
+package cl.bgmp.staff.staffmode.modules;
+
+import com.google.inject.Singleton;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+@Singleton
+public class MuteChatModule extends StaffModeModule {
+  private boolean muted = false;
+
+  public void muteChatBy(Player muter) {
+    if (this.muted) {
+      Bukkit.broadcastMessage(
+          ChatColor.GREEN + "Chat has been unmuted by " + ChatColor.RESET + muter.getName());
+    } else {
+      Bukkit.broadcastMessage(
+          ChatColor.RED + "Chat has been muted by " + ChatColor.RESET + muter.getName());
+    }
+
+    this.muted = !this.muted;
+  }
+
+  @EventHandler(priority = EventPriority.HIGH)
+  public void onPlayerChat(AsyncPlayerChatEvent event) {
+    if (!this.muted) return;
+
+    event.setCancelled(true);
+    event.getPlayer().sendMessage(ChatColor.RED + "Chat is muted!");
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
